8df2b9e943171eebc665c0e04d64fd37a9600cd8
ivtsylin <tsylin.iv@gmail.com>
2023-03-26 23:50:38 +0300

    first_task

---
+++ 01-White-Belt/01-First-week/01-Sum/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Belts_challenge
+//
+//  Created by Ivan Tsylin on 05.01.2023.
+//
+
+#include <iostream>
+#include <map>
+//#include "WhiteBeltProblems.hpp"
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
