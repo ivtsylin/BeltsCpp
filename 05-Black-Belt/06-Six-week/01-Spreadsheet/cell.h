95eb03040699b467ab9b78ca0006c2f6325d0d74
ivtsylin <tsylin.iv@gmail.com>
2024-08-26 23:48:40 +0300

    Black6w

---
+++ 05-Black-Belt/06-Six-week/01-Spreadsheet/cell.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "common.h"
+#include "formula.h"
+
+#include <functional>
+#include <unordered_set>
+
+class Sheet;
+
+class Cell : public ICell {
+public:
+  Cell(Sheet& sheet);
+  ~Cell();
+
+  void Set(std::string text);
+  void Clear();
+
+  Value GetValue() const override;
+  std::string GetText() const override;
+  std::vector<Position> GetReferencedCells() const override;
+
+  void UpdateFormula(
+      const std::function<IFormula::HandlingResult(IFormula&)>& update);
+
+  bool IsReferenced() const;
+
+private:
+  class Impl;
+  class EmptyImpl;
+  class TextImpl;
+  class FormulaImpl;
+
+  bool WouldIntroduceCircularDependency(const Impl& newImpl) const;
+  void UpdateRefs();
+  void InvalidateCacheRecursive(bool force = false);
+
+  std::unique_ptr<Impl> impl_;
+  Sheet& sheet_;
+  std::unordered_set<Cell*> incomingRefs_;
+  std::unordered_set<Cell*> outgoingRefs_;
+};
---
+++ 05-Black-Belt/06-Six-week/01-Spreadsheet/sheet.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <functional>
+
+class Sheet : public ISheet {
+public:
+  ~Sheet();
+
+  void SetCell(Position pos, std::string text) override;
+
+  const ICell* GetCell(Position pos) const override;
+  ICell* GetCell(Position pos) override;
+
+  void ClearCell(Position pos) override;
+
+  void InsertRows(int before, int count) override;
+  void InsertCols(int before, int count) override;
+  void DeleteRows(int first, int count) override;
+  void DeleteCols(int first, int count) override;
+
+  Size GetPrintableSize() const override;
+
+  void PrintValues(std::ostream& output) const override;
+  void PrintTexts(std::ostream& output) const override;
+
+  const Cell* GetConcreteCell(Position pos) const;
+  Cell* GetConcreteCell(Position pos);
+
+private:
+  void MaybeIncreaseSizeToIncludePosition(Position pos);
+  void PrintCells(std::ostream& output,
+                  const std::function<void(const ICell&)>& printCell) const;
+  void UpdateFormulas(
+      const std::function<IFormula::HandlingResult(IFormula&)>& update);
+  Size GetActualSize() const;
+
+  std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+};
